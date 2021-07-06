62757e1f633dedbca2939cd005418d88eb41a492
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-06 21:12:01 +0200

    aggiunta restituzione dell'id da parte del web service

--- src/main/java/com/synthshowcase/SynthesizersShowcase/SynthesizersShowcaseApplication.java
+++ src/main/java/com/synthshowcase/SynthesizersShowcase/SynthesizersShowcaseApplication.java
@@ -1,13 +1,23 @@
 package com.synthshowcase.SynthesizersShowcase;
 
+import com.synthshowcase.SynthesizersShowcase.model.Synthesizer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
-public class SynthesizersShowcaseApplication {
+public class SynthesizersShowcaseApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SynthesizersShowcaseApplication.class, args);
 	}
+
+	@Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Synthesizer.class);
+    }
 
 }
